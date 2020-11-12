2bd718fc0c8c0dbdf2eb7bcaf47fb53cd4364a38
Brian Demro <demrobrian@hotmail.com>
2020-11-12 15:05:26 -0600

    updating

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HashRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
