594b1095714d8c409d3e9ef54b53a53518bdfbf6
Brian Demro <demrobrian@hotmail.com>
2020-11-12 13:32:24 -0600

    part one of login function in login

--- src/componenents/Auth.js
+++ src/componenents/Auth.js
@@ -1,10 +1,103 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 
 class Auth extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            username: '',
+            password: '',
+            newUser: false
+        }
+    }
+
+    toggleNewUser=() => {
+        this.setState({
+            newUser: !this.state.newUser
+        })
+    };
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    login = async (e) => {
+        e.preventDefault();
+        const {email, password} = this.state;
+        try{
+            const user = await axios.post('/auth/login', {email, password})
+            alert(user)
+            this.props.history.push('/feed')
+        }
+        catch(err) {
+             alert(err.response.request.response)
+        }
+    };
+
+    register = async (e) => {
+        e.preventDefault();
+        const {email, password, username} = this.state;
+        try{
+            const user = await axios.post('/auth/register', {email, password, username})
+            alert(user)
+            this.props.history.push('/feed')
+        }
+        catch(err) {
+             alert(err.response.request.response)
+        }
+    };
+
     render() {
+        const {email, password, username} = this.state;
         return (
             <div>
-               this is the Auth Component 
+               {this.state.newUser ?
+               <div>
+                   <h3>Register</h3>
+                   <form onSubmit={e => this.register(e)}>
+                       <input 
+                            name="email" 
+                            value={email} 
+                            placeholder="Email" 
+                            onChange={e=>this.changeHandler(e)}
+                        />
+                       <input 
+                            name="username" 
+                            value={username}
+                             placeholder="Username"
+                              onChange={e=>this.changeHandler(e)}
+                        />
+                       <input 
+                            name="password" 
+                            value={password} 
+                            placeholder="Password"
+                             onChange={e=>this.changeHandler(e)}
+                        />
+                   </form>
+                   <button onClick={this.toggleNewUser}>Already a user?</button>
+               </div>
+               :
+               <div>
+                   <h3>Login</h3>
+                   <form onSubmit= {e =>this.login(e)}>
+                       <input 
+                            name="email" 
+                            value={email} 
+                            placeholder="Email" 
+                            onChange={e=>this.changeHandler(e)}
+                        />
+                       <input 
+                            name="password" 
+                            value={password} 
+                            placeholder="Password"
+                             onChange={e=>this.changeHandler(e)}
+                        />
+                   </form>
+                   <button onClick = {this.toggleNewUser}>Need an account?</button>
+               </div>} 
             </div>
         )
     }
